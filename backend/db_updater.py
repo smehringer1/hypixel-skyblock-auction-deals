70822c85e6772bccc2bb938a6177099d6adc9cc7
smehringer <spencermehringer@gmail.com>
2020-09-24 16:36:22 -0400

    dockerize

--- backend/db_updater.py
+++ backend/db_updater.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-connection = pymongo.MongoClient(os.getenv('MONGO_LOCAL'))
+connection = pymongo.MongoClient(os.getenv('MONGO_DOCKER'))
 db = connection['db']
 auctionDB = db['auctions']
 lowestPricesDB = db['lowest_prices']
