8e2e7c907d5a55090e81e6e08991586dc1c5fce5
smehringer <spencermehringer@gmail.com>
2020-10-03 19:58:16 -0400

    dockerized

--- fetcher/db_updater.py
+++ fetcher/db_updater.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-connection = pymongo.MongoClient(os.getenv('MONGO_DOCKER'))
+connection = pymongo.MongoClient(os.getenv('MONGO'))
 db = connection['auctionDB']
 auctionDB = db['auctions']
 lowestPricesDB = db['lowest_prices']
--- flask/initialize.py
+++ flask/initialize.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-connection = pymongo.MongoClient(os.getenv('MONGO_LOCAL'))
+connection = pymongo.MongoClient(os.getenv('MONGO'))
 db = connection['auctionDB']
 auctionDB = db['auctions']
 lowestPricesDB = db['lowest_prices']
